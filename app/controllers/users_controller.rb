33bb94852511e24d316ff8319bd0d67f9642e83a
Joshua-McGee <jcooler.cooler@gmail.com>
2019-09-19 20:16:28 +0000

    added login and logout functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  # new actions for the signup form
+  def new
+  end
+
+  # create action for recieving the form and creating a user with the forms parameters
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+end
